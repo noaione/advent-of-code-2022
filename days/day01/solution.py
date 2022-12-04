3176f39efa246376c8f91539972bded0bc65bfb2
noaione <noaione0809@gmail.com>
2022-12-04 13:25:17 +0800

    day01: allow for parse benchmarking

--- days/day01/solution.py
+++ days/day01/solution.py
@@ -3,19 +3,19 @@ from pathlib import Path
 CURRENT_DIR = Path(__file__).absolute().parent
 
 
-def common(puzzle: str):
+def parse(puzzle: str):
     split_doubles = puzzle.split("\n\n")
     all_numbers = [sum([int(number) for number in group.split("\n") if number]) for group in split_doubles]
     return all_numbers
 
 
 def part_a(puzzle: str) -> str:
-    all_numbers = common(puzzle)
+    all_numbers = parse(puzzle)
     return str(max(all_numbers))
 
 
 def part_b(puzzle: str):
-    all_numbers = sorted(common(puzzle), reverse=True)
+    all_numbers = sorted(parse(puzzle), reverse=True)
     return str(sum(all_numbers[:3]))
 
 
